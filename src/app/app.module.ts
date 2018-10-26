f200cb93549c90d4488f6511597efc43970dad20
Michal Nowicki <mimonowi@wp.pl>
2018-10-26 14:08:41 +0200

    Dodanie routingu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent,
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    QuizComponent
+    QuizComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
