1aba14051590dbc3a04eedfbabd0c1ade0a27435
Michal Nowicki <mimonowi@wp.pl>
2018-10-26 13:06:17 +0200

    Poczatek

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule
