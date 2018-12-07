bc990e669934cbd0675bfb2ec4a78ae50647aff8
Michal Nowicki <mimonowi@wp.pl>
2018-12-07 15:59:47 +0100

    Baza danych dziala/ blad servera przy przesylaniu danych cz2

---
+++ api/app/business/user.manager.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import PasswordDAO from '../DAO/passwordDAO';
+import TokenDAO from '../DAO/tokenDAO';
+import UserDAO from '../DAO/userDAO';
+import applicationException from '../service/applicationException';
+import sha1 from 'sha1';
+
+function create(context) {
+
+  function hashString(password) {
+    return sha1(password);
+  }
+
+  async function authenticate(name, password) {
+    let userData;
+    const user = await UserDAO.getByEmailOrName(name);
+    if (!user) {
+      throw applicationException.new(applicationException.UNAUTHORIZED, 'User with that email does not exist');
+    }
+    if (!user.active) {
+      throw applicationException.new(applicationException.NOT_FOUND, 'User does not exist or does not active');
+    }
+    userData = await user;
+    await PasswordDAO.authorize(user.id, hashString(password));
+    const token = await TokenDAO.create(userData);
+    return getToken(token);
+  }
+
+  function getToken(token) {
+    return {token: token.value};
+  }
+
+
+  return {
+    authenticate: authenticate
+
+  };
+}
+
+export default {
+  create: create
+};
